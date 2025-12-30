6d14bc8f00939b4c1b3480f4ce1bc2af9f2f2c50
Peter Domorev <peter@domorev.com>
2025-12-30 16:06:18 +1100

    Calculated averages and % busts

--- FarkleSim/Program.cs
+++ FarkleSim/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FarkleSim
 {
@@ -10,6 +11,7 @@ namespace FarkleSim
 
             Conservative1 consPlayer = new Conservative1();
 
+            /*
             int numTurns = 10000;
             int[] scores = new int[numTurns];
 
@@ -21,12 +23,77 @@ namespace FarkleSim
             }
 
             double mean = scores.Average();
-            Console.WriteLine($"Mean: {mean}");
+            Console.WriteLine($"Mean: {mean}");*/
+
+
+            // make 6 dice
+            List<Dice> dice = new List<Dice>();
+            int maxNumDice = 6;
+            for (int i = 0; i < maxNumDice; i++)
+            {
+                dice.Add(new Dice());
+            }
+
+            // set up to collect data
+            int numRuns = 1000000;
+            double[] means = new double[maxNumDice];
+            double[] stds = new double[maxNumDice];
+            double[] percentBust = new double[maxNumDice];
+            double[] numBust = new double[maxNumDice];
 
 
 
 
-            
+            for (int j = 0; j < 6; j++) // simulate each number of dice
+            {
+                int[] maxScores = new int[numRuns];
+                int numDice = dice.Count;   // 6,5,4,3,2,1
+
+
+                // errors, valueCalc modifies the dice reference
+                // incorrect indexing (for loop vs num dice)
+
+                for (int i = 0; i < numRuns; i++) // simulate rolls
+                {
+                    foreach (Dice singleDice in dice) singleDice.Roll(); // roll
+
+
+                    // calculate max value, add to maxScores
+                    TradeOptions tradeOptions = valueCalc.Calculate(dice.ToList());
+                    if(tradeOptions.Options.Count == 0)
+                    {
+                        maxScores[i] = 0;
+                        continue;
+                    }
+                    maxScores[i] = tradeOptions.Options.Max(t => t.Score);
+                }
+
+
+                // calculate data from set of dice, add
+                double mean = maxScores.Average();
+                double std = Math.Sqrt(maxScores.Average(v => (v - mean) * (v - mean)));
+                means[numDice - 1] = mean;
+                stds[numDice - 1] = std;
+
+                double pBust = (double)maxScores.Count(t => t == 0) / numRuns;
+                percentBust[numDice - 1] = pBust;
+
+                numBust[numDice - 1] = maxScores.Count(t => t == 0);
+
+                dice.RemoveAt(dice.Count - 1);
+            }
+
+
+            for (int numDice = 1; numDice <= maxNumDice; numDice++)
+            {
+                Console.WriteLine($"Number of dice: {numDice}, Mean: {means[numDice - 1]}, Std: {stds[numDice - 1]}");
+                Console.WriteLine($"Percentage bust: {percentBust[numDice - 1]}");
+            }
+
+
+
+
+
 
 
 
@@ -74,7 +141,7 @@ namespace FarkleSim
 
 
 
-     
+
 
 
 
